840e87f03fbd32b78b737cbe8e42384d4caa1f16
Hiperultimate <hiperutlimatevegito@gmail.com>
2023-05-01 16:35:29 +0530

    Fixed an svg length auto error which was being displayed in console.

--- public/SvgGroupyLogo.tsx
+++ public/SvgGroupyLogo.tsx
@@ -6,7 +6,6 @@ const SvgGroupyLogo = (props: ComponentProp) => (
   <svg
     xmlns="http://www.w3.org/2000/svg"
     width="100%"
-    height="auto"
     fill="none"
     viewBox="0 0 36 36"
     {...props}
