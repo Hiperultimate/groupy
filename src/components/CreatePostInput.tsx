3490cfd1408f99ddbc5e361060ca92048e1898da
Hiperultimate <hiperutlimatevegito@gmail.com>
2023-05-29 14:25:51 +0530

    Styled CreatePostInput component

---
+++ src/components/CreatePostInput.tsx
@@ -0,0 +1,24 @@
+import DisplayUserImage from "./DisplayUserImage";
+
+/* On clicking the input field, open a modal which has all the form options to create a post */
+const CreatePostInput = ({ userImage }: { userImage: string | null | undefined}) => {
+  return (
+    <div className="flex w-[545px] mx-3 rounded-lg bg-white p-3 font-poppins shadow-md">
+      <div>
+        <DisplayUserImage userImage={userImage} sizeOption="small" />
+      </div>
+      <input
+        className="ml-3 w-full rounded-full border-2 pl-5 pr-6"
+        type="text"
+        name="createPostInput"
+        value=""
+        placeholder="Create a post &rarr;"
+        onChange={() => {
+          console.log("Placeholder");
+        }}
+      />
+    </div>
+  );
+};
+
+export default CreatePostInput;
