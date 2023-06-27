cf9b573e9021db8260abc38987bc589bb0bd8521
Hiperultimate <hiperultimatevegito@gmail.com>
2023-06-27 18:35:14 +0530

    Getting posts from backend in serverside

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -2,6 +2,7 @@ import { createTRPCRouter } from "~/server/api/trpc";
 import { exampleRouter } from "~/server/api/routers/example";
 import { authRouter } from "./routers/authenticate";
 import { tags } from "./routers/tags";
+import { postRouter } from "./routers/posts";
 
 /**
  * This is the primary router for your server.
@@ -12,6 +13,7 @@ export const appRouter = createTRPCRouter({
   example: exampleRouter,
   account: authRouter,
   tags: tags,
+  post: postRouter,
 });
 
 // export type definition of API
