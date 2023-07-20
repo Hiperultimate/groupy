452d481863c356aed7d961125e8d4b740f095a1b
Hiperultimate <hiperultimatevegito@gmail.com>
2023-07-20 17:01:03 +0530

    Added a new friendship table and made and made a relation in user table.

---
+++ prisma/migrations/20230720065145_friendlist/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "Friendship" (
+    "id" TEXT NOT NULL,
+    "friendId" TEXT NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+
+    CONSTRAINT "Friendship_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Friendship" ADD CONSTRAINT "Friendship_friendId_fkey" FOREIGN KEY ("friendId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
