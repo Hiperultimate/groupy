16be8848292d515027a69298fff5180031977d91
Hiperultimate <hiperultimatevegito@gmail.com>
2023-12-22 14:38:36 +0530

    Initializing chatting page

---
+++ src/pages/chat.tsx
@@ -0,0 +1,11 @@
+import { type NextPage } from "next";
+
+const Chat: NextPage = () => {
+  return (
+    <main className="pt-20">
+      <h3>Chatting Page</h3>
+    </main>
+  );
+};
+
+export default Chat;
