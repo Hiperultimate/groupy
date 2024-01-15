9d51ea4c1869ab35d1187f23b732cba3fe88cbd7
Hiperultimate <hiperultimatevegito@gmail.com>
2024-01-15 20:50:48 +0530

    Created and styled ChatTextInput component

--- src/components/ChatComponent/ChatTextInput.tsx
+++ src/components/ChatComponent/ChatTextInput.tsx
@@ -1,5 +1,24 @@
+import SvgChatIcon from "public/SvgChatIcon";
+import { useState } from "react";
+
 const ChatTextInput = () => {
-  return <>Chat Input</>;
+  const [chatInput, setChatInput] = useState("");
+  return (
+    <div className="flex h-full items-center justify-center border-t">
+      <div className="flex h-11 w-full items-center justify-center rounded-full border">
+        <input
+          type="text"
+          value={chatInput}
+          onChange={(e) => setChatInput(e.target.value)}
+          className="mx-5 w-full text-lg outline-none"
+        />
+        <span className="h-full border-l" />
+        <div className="relative top-[2px] pl-3 pr-4 hover:cursor-pointer">
+          <SvgChatIcon />
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default ChatTextInput;
