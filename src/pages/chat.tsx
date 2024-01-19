9f36ada483f2ee1a19f9529f321c44f47c9b2ab2
Hiperultimate <hiperultimatevegito@gmail.com>
2024-01-19 16:32:43 +0530

    Reverting changes

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -5,15 +5,10 @@ import UserChatList from "~/components/ChatComponent/UserChatList";
 const Chat: NextPage = () => {
   return (
     <main className="flex pt-20 font-poppins">
-      <div className="absolute top-2  flex h-full w-full items-center justify-center">
-        <span className="background-animate z-50 rounded-md p-4 bg-cyan-600 text-white">
-          ! Work in progress !
-        </span>
-      </div>
-      <div className="w-3/12 border blur">
+      <div className="w-3/12 border">
         <UserChatList />
       </div>
-      <div className="w-9/12 border border-l-0 blur">
+      <div className="w-9/12 border border-l-0">
         <ChatArea />
       </div>
     </main>
