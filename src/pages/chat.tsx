c9eb8babc45e182605dad694172e9053cccb080e
Hiperultimate <hiperultimatevegito@gmail.com>
2024-01-30 14:09:34 +0530

    chat page in progress

--- src/pages/chat.tsx
+++ src/pages/chat.tsx
@@ -14,10 +14,15 @@ const Chat: NextPage = () => {
         editType={editModalData.editType}
       />
       <main className="flex pt-20 font-poppins">
-        <div className="w-3/12 border">
+        <div className="absolute top-2  flex h-full w-full items-center justify-center">
+          <span className="background-animate z-50 rounded-md bg-cyan-600 p-4 text-white">
+            ! Work in progress !
+          </span>
+        </div>
+        <div className="w-3/12 border blur">
           <UserChatList />
         </div>
-        <div className="w-9/12 border border-l-0">
+        <div className="w-9/12 border border-l-0 blur">
           <ChatArea />
         </div>
       </main>
