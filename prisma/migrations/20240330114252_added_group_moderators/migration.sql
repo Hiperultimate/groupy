b8bf58bb1880df2f3613e13f13a1674e5b6fceed
Hiperultimate <hiperultimatevegito@gmail.com>
2024-03-30 17:13:18 +0530

    Prisma schema to add or remove moderators for a group

---
+++ prisma/migrations/20240330114252_added_group_moderators/migration.sql
@@ -0,0 +1,17 @@
+-- CreateTable
+CREATE TABLE "_GroupToUser" (
+    "A" TEXT NOT NULL,
+    "B" TEXT NOT NULL
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_GroupToUser_AB_unique" ON "_GroupToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_GroupToUser_B_index" ON "_GroupToUser"("B");
+
+-- AddForeignKey
+ALTER TABLE "_GroupToUser" ADD CONSTRAINT "_GroupToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "Group"("id") ON DELETE CASCADE ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "_GroupToUser" ADD CONSTRAINT "_GroupToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User"("id") ON DELETE CASCADE ON UPDATE CASCADE;
