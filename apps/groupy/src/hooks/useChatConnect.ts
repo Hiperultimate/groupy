63fefe50429fb287f80646594672b270528b018a
Hiperultimate <hiperultimatevegito@gmail.com>
2024-04-28 16:31:33 +0530

    Groupy now listen to error messages emitted by chatServer

--- apps/groupy/src/hooks/useChatConnect.ts
+++ apps/groupy/src/hooks/useChatConnect.ts
@@ -1,12 +1,17 @@
 import { useEffect } from "react";
+import { toast } from "react-toastify";
 import { socket } from "~/utils/socket";
 
 // Hook to connect to the chat server
 const useChatConnect = () => {
   useEffect(() => {
     socket.connect();
+    socket.on("error", (message : string) => {
+      toast.error(message);
+    })
 
     return () => {
+      socket.off("error");
       socket.disconnect();
     };
   }, []);
