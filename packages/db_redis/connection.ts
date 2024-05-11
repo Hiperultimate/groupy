3cdf832c83841ad557a7ed07f4b0ecf0f18b7d7b
Hiperultimate <hiperultimatevegito@gmail.com>
2024-05-11 14:42:14 +0530

    Fixing env path and naming redis connection file

---
+++ packages/db_redis/connection.ts
@@ -0,0 +1,9 @@
+import { Redis } from "ioredis";
+import { env } from "./env";
+
+export const redis = new Redis({
+  port: Number(env.REDIS_PORT), // Redis port
+  host: String(env.REDIS_HOST), // Redis host
+  username: "default", // needs Redis >= 6
+  password: env.REDIS_PASSWORD,
+});
