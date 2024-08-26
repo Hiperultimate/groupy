10d5531917a05e912faccc5b43891e92ab37f475
Hiperultimate <hiperultimatevegito@gmail.com>
2024-08-26 17:13:18 +0530

    Added zod validation for prisma package to accept test db url

--- packages/db_prisma/index.ts
+++ packages/db_prisma/index.ts
@@ -1,1 +1,43 @@
-export * from '@prisma/client';
+import dotenv from "dotenv";
+import { z } from "zod";
+
+dotenv.config();
+
+const prismaEnvSchema = z.object({
+  NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+  DATABASE_URL: z.string().url(),
+});
+
+let finalPrismaEnv = prismaEnvSchema;
+
+switch (process.env.NODE_ENV) {
+  case "test":
+    finalPrismaEnv = finalPrismaEnv.extend({
+      TEST_DATABASE_URL: z.string().url(),
+    });
+    break;
+  case "production":
+    break;
+  case "development":
+    console.log("Checking DEVELOPMENT DEFAULT");
+    break;
+}
+
+const checkErrors = finalPrismaEnv.safeParse(process.env);
+console.log("Checking for errors in prisma :", checkErrors);
+if (checkErrors.success === false) {
+  console.error(
+    "❌ Invalid environment variables in packages/db_prisma:",
+    checkErrors.error.flatten().fieldErrors
+  );
+  throw new Error("Invalid environment variables");
+}
+
+// Replacing DATABASE_URL with TEST_DATABASE_URL for prisma
+if (process.env.NODE_ENV === "test") {
+  process.env.DATABASE_URL = process.env.TEST_DATABASE_URL;
+}
+
+export * from "@prisma/client";
