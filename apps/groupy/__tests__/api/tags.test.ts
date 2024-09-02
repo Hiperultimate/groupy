cd5b04d5a81af9eacc0b9a33702c7f7c1d812c25
Hiperultimate <hiperultimatevegito@gmail.com>
2024-09-02 18:35:48 +0530

    Added unit test for relatedTag

--- apps/groupy/__tests__/api/tags.test.ts
+++ apps/groupy/__tests__/api/tags.test.ts
@@ -1,6 +1,6 @@
 // @vitest-environment node
 
-import { type inferProcedureInput } from "@trpc/server";
+import type { inferProcedureOutput, inferProcedureInput } from "@trpc/server";
 import { beforeEach, describe, it, vi } from "vitest";
 import { expect } from "vitest";
 import prismaMock from "~/server/__mocks__/db";
@@ -8,10 +8,13 @@ import prismaMock from "~/server/__mocks__/db";
 import { appRouter, type AppRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
 
+type Input = inferProcedureInput<AppRouter["tags"]["relatedTags"]>;
+type Output = inferProcedureOutput<AppRouter["tags"]["relatedTags"]>;
+
 describe("relatedTags", () => {
   beforeEach(() => {
     vi.restoreAllMocks();
-  })
+  });
 
   const ctx = createInnerTRPCContext({ session: null });
   const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
@@ -23,8 +26,17 @@ describe("relatedTags", () => {
     ];
     prismaMock.tag.findMany.mockResolvedValue(mockTags);
 
-    type Input = inferProcedureInput<AppRouter["tags"]["relatedTags"]>;
     const input: Input = "fi";
+
+    const getRelatedTags = await caller.tags.relatedTags(input);
+    expect(getRelatedTags).toMatchObject(mockTags);
+  });
+
+  it("Returns empty array if relatedTag not found", async () => {
+    const mockTags : Output = [];
+    prismaMock.tag.findMany.mockResolvedValue(mockTags);
+
+    const input: Input = "uniqueTag";
 
     const getRelatedTags = await caller.tags.relatedTags(input);
     expect(getRelatedTags).toMatchObject(mockTags);
