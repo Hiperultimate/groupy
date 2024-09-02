e60b2e1e59d16252e4a4bb24e8d442708678d7bf
Hiperultimate <hiperultimatevegito@gmail.com>
2024-09-02 18:30:02 +0530

    Initiated relatedTags unit testing with mocked database

---
+++ apps/groupy/__tests__/api/tags.test.ts
@@ -0,0 +1,32 @@
+// @vitest-environment node
+
+import { type inferProcedureInput } from "@trpc/server";
+import { beforeEach, describe, it, vi } from "vitest";
+import { expect } from "vitest";
+import prismaMock from "~/server/__mocks__/db";
+
+import { appRouter, type AppRouter } from "~/server/api/root";
+import { createInnerTRPCContext } from "~/server/api/trpc";
+
+describe("relatedTags", () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  })
+
+  const ctx = createInnerTRPCContext({ session: null });
+  const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
+
+  it("Fetches related tags using relatedTag procedure", async () => {
+    const mockTags = [
+      { id: "b4125b3452w34fcqwe", name: "hellfire" },
+      { id: "qwce245b1v4523b14", name: "fireflow" },
+    ];
+    prismaMock.tag.findMany.mockResolvedValue(mockTags);
+
+    type Input = inferProcedureInput<AppRouter["tags"]["relatedTags"]>;
+    const input: Input = "fi";
+
+    const getRelatedTags = await caller.tags.relatedTags(input);
+    expect(getRelatedTags).toMatchObject(mockTags);
+  });
+});
