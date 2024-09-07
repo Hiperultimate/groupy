5c6ae260d753751c5aa4767f95c61e90af2baa8f
Hiperultimate <hiperultimatevegito@gmail.com>
2024-09-07 14:07:25 +0530

    Added test for post API and cleaned code.

--- apps/groupy/__tests__/api/posts.test.ts
+++ apps/groupy/__tests__/api/posts.test.ts
@@ -1,37 +1,38 @@
 // @vitest-environment node
 
-import { beforeEach, describe, expect, it, vi } from "vitest";
+import { beforeAll, beforeEach, describe, expect, it, vi } from "vitest";
 import prismaMock from "~/server/__mocks__/db";
-import { type AppRouter, appRouter } from "~/server/api/root";
+import { appRouter } from "~/server/api/root";
 import { createInnerTRPCContext } from "~/server/api/trpc";
-import type { Post, Prisma } from "@prisma/client";
+import type { Prisma } from "@prisma/client";
 import { type Session } from "next-auth";
 import { postFindOne } from "__tests__/__factories__/post";
+import { TRPCError } from "@trpc/server";
 
 describe("getPosts", () => {
+  beforeAll(() => {
+    vi.mock("~/utils/storageBucket", () => {
+      return {
+        supabase: {
+          storage: {
+            from: vi.fn((imagePath: string) => ({
+              getPublicUrl: vi.fn((imageName: string) => {
+                return {
+                  data: {
+                    publicUrl: `https://supabaseURL.co/storage/v1/object/public/${imagePath}/${imageName}`,
+                  },
+                };
+              }),
+            })),
+          },
+        },
+      };
+    });
+  });
+
   beforeEach(() => {
     vi.restoreAllMocks();
   });
-
-  vi.mock("~/utils/storageBucket", () => {
-    return {
-      supabase: {
-        storage: {
-          from: vi.fn((imagePath: string) => ({
-            getPublicUrl: vi.fn((imageName: string) => {
-              return {
-                data: {
-                  publicUrl: `https://supabaseURL.co/storage/v1/object/public/${imagePath}/${imageName}`,
-                },
-              };
-            }),
-          })),
-        },
-      },
-    };
-  });
-
-  const postId = "51lik5tiug123tiu";
 
   const testSession: Session = {
     user: {
@@ -50,6 +51,18 @@ describe("getPosts", () => {
   const ctx = createInnerTRPCContext({ session: testSession });
   const caller = appRouter.createCaller({ ...ctx, prisma: prismaMock });
 
+  it("Should throw UNAUTHORIZED error if session is invalid ", async () => {
+    const nullSessionCtx = createInnerTRPCContext({ session: null });
+    const testCaller = appRouter.createCaller({
+      ...nullSessionCtx,
+      prisma: prismaMock,
+    });
+
+    await expect(testCaller.post.getPosts({ takenPosts: 0 })).rejects.toThrow(
+      new TRPCError({ code: "UNAUTHORIZED" })
+    );
+  });
+
   it("getPosts api returning true if current user has liked the post ", async () => {
     type GetPostOutput = Prisma.PostGetPayload<{
       include: {
@@ -59,6 +72,61 @@ describe("getPosts", () => {
       };
     }>;
 
+    const postId = "51lik5tiug123tiu";
+    const datePlaceholder = new Date(2015, 11, 19);
+    const prismaPostFindManyMock: GetPostOutput[] = [
+      postFindOne({
+        id: postId,
+        image: null,
+        likedBy: [
+          {
+            userId: "15uih1234",
+            createdAt: datePlaceholder,
+            id: "21h5g12jk",
+            postId: "iu12hg5",
+            updatedAt: datePlaceholder,
+          },
+        ],
+      }),
+    ];
+
+    const result = [
+      {
+        id: postId,
+        content: "Default Content",
+        image: null,
+        authorId: "deafult-author-id",
+        groupId: null,
+        groupSize: null,
+        createdAt: datePlaceholder,
+        updatedAt: datePlaceholder,
+        tags: [
+          { id: "default-tag-id-1", name: "default-tag-1" },
+          { id: "default-tag-id-2", name: "default-tag-2" },
+        ],
+        likeCount: 1,
+        isUserLikePost: true,
+        commentCount: 1,
+      },
+    ];
+
+    prismaMock.post.findMany.mockResolvedValue(prismaPostFindManyMock);
+    const postData = await caller.post.getPosts({ takenPosts: 0 });
+
+    expect(postData.every((post) => post.isUserLikePost)).toBeTruthy();
+    expect(postData).toMatchObject(result);
+  });
+
+  it("getPosts api generating proper public url for image", async () => {
+    type GetPostOutput = Prisma.PostGetPayload<{
+      include: {
+        tags: true;
+        comments: true;
+        likedBy: true;
+      };
+    }>;
+
+    const postId = "51lik5tiug123tiu";
     const datePlaceholder = new Date(2015, 11, 19);
     const prismaPostFindManyMock: GetPostOutput[] = [
       postFindOne({
@@ -96,9 +164,10 @@ describe("getPosts", () => {
         commentCount: 1,
       },
     ];
-    
+
     prismaMock.post.findMany.mockResolvedValue(prismaPostFindManyMock);
     const postData = await caller.post.getPosts({ takenPosts: 0 });
-    expect(postData).toMatchObject(result);
+
+    expect(postData[0]?.image).toBe(result[0]?.image);
   });
 });
