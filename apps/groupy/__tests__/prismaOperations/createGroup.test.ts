f327efa1e172fa7dddc28883f3e1d5b6f6c47b6e
Hiperultimate <hiperultimatevegito@gmail.com>
2024-09-19 12:21:00 +0530

    Added test for createGroup prismaOperation

--- apps/groupy/__tests__/prismaOperations/createGroup.test.ts
+++ apps/groupy/__tests__/prismaOperations/createGroup.test.ts
@@ -1,15 +1,18 @@
 import { TRPCError } from "@trpc/server";
 import { createGroupData } from "../__factories__/post";
-import { describe, expect, it } from "vitest";
+import { beforeEach, describe, expect, it, vi } from "vitest";
 import prismaMock from "~/server/__mocks__/db";
 import createGroup from "~/server/prismaOperations/createGroup";
 
 describe("createGroup", () => {
+  beforeEach(() => {
+    vi.restoreAllMocks();
+  });
+
   it("Expect error code BAD_REQUEST if isGroup is true and group name is not provided ", async () => {
     const groupInput = createGroupData({
       name: "",
     });
-    // prismaMock.group.create.mockResolvedValue()
 
     await expect(
       createGroup({
@@ -28,5 +31,29 @@ describe("createGroup", () => {
         code: "BAD_REQUEST",
       })
     );
+  });
+
+  it("Returns serialized post data if createPost API worked", async () => {
+    const groupInput = createGroupData();
+    prismaMock.group.create.mockResolvedValue(groupInput);
+    prismaMock.unreadMessage.create.mockResolvedValue({
+      userId: "1231241",
+      groupId: "21414512",
+      id: "214124",
+      unreadMessageCount: 23,
+    });
+
+    await expect(
+      createGroup({
+        prisma: prismaMock,
+        groupMakerId: groupInput.id,
+        groupName: groupInput.name,
+        groupImage: groupInput.image,
+        minAgeLimit: groupInput.minAgeLimit,
+        maxAgeLimit: groupInput.maxAgeLimit,
+        size: groupInput.size,
+        instantJoin: groupInput.instantJoin,
+      })
+    ).resolves.toBe(groupInput);
   });
 });
