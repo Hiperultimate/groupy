f379e8e087dd04746483834d6966e5fba65b515c
Hiperultimate <hiperultimatevegito@gmail.com>
2024-10-30 14:31:58 +0530

    Refactored signup.cy code and made it more readable

--- apps/groupy/cypress/e2e/signup.cy.ts
+++ apps/groupy/cypress/e2e/signup.cy.ts
@@ -1,24 +1,6 @@
 /// <reference types="cypress" />
 
-function returnCypressDate(date: Date) {
-  return `${date.getFullYear()}-${String(date.getMonth()).padStart(
-    2,
-    "0"
-  )}-${String(date.getMonth()).padStart(2, "0")}`;
-}
-
 describe("signup-page-form", () => {
-  const getNameField = () => cy.getByData("name-input");
-  const getEmailField = () => cy.getByData("email-input");
-  const getPasswordField = () => cy.getByData("password-input");
-  const getConfirmPasswordField = () => cy.getByData("confirmPassword-input");
-  const getDobField = () => cy.getByData("dob-input");
-  const getTagNameField = () => cy.getByData("tagName-input");
-  const getUserDescriptionField = () => cy.getByData("userDescription-input");
-  const getTagSelectField = () => cy.getById("tagSelect-input");
-  const getImageField = () => cy.getByData("image-input");
-  const getFormSubmitButton = () => cy.getByData("signupSubmit-btn");
-
   it("login page signup button redirects to signup page", () => {
     cy.visit("/");
 
@@ -53,68 +35,15 @@ describe("signup-page-form", () => {
   it("Checking errors of input fields invalid input errors", () => {
     cy.visit("/sign-up");
 
-    // Getting form input fields
-    const nameField = getNameField();
-    const emailField = getEmailField();
-    const passwordField = getPasswordField();
-    const confirmPasswordField = getConfirmPasswordField();
-    const dobField = getDobField();
-    const tagNameField = getTagNameField();
-    const userDescriptionField = getUserDescriptionField();
-    const tagSelectField = getTagSelectField();
-    const imageField = getImageField();
-    const formSubmitButton = getFormSubmitButton();
-
-    // Setting value for form inputs
-    const futureDate = new Date();
-    futureDate.setFullYear(futureDate.getFullYear() + 1);
-
-    nameField.type("Mi");
-    emailField.type("jo.com@gmail");
-    passwordField.type("123123123123123123123");
-    confirmPasswordField.type("1234567");
-    dobField.type(returnCypressDate(futureDate));
-    tagNameField.type("mi");
-    userDescriptionField.type("Optional");
-    tagSelectField.type("Tag 1{enter}");
-    tagSelectField.type("Tag 2{enter}");
-    imageField.selectFile("cypress/fixtures/images/bad-image.iso", {
-      force: true,
-    });
-    formSubmitButton.click();
-
-    //Checking if the error messages are as required
-    const expectedFieldError = [
-      "String must contain at least 3 character(s)",
-      "Invalid email",
-      "String must contain at most 16 character(s)",
-      "String must contain at least 8 character(s)",
-      "Passwords do not match",
-      "Birth date must be in the past",
-      "String must contain at least 3 character(s)",
-      "You must select at least 3 tags.",
-      "Invalid file",
-      "Invalid image format",
-    ];
-    const inputErrors = cy.getByData("input-error");
-
-    inputErrors.each(($el, index) => {
-      cy.wrap($el)
-        .invoke("text")
-        .then((text) => {
-          cy.log(`Error ${index + 1}: ${text}`);
-        });
-    });
-
-    inputErrors.each(($el, index) => {
-      cy.wrap($el).invoke("text").should("eq", expectedFieldError[index]);
-    });
+    const expectedFieldErrorArr = fillInvalidSignupInput();
+    getFormSubmitButton().click();
+    checkErrorMessages(cy.getByData("input-error"), expectedFieldErrorArr);
 
     // Testing file size restraint for image input
-    imageField.selectFile("cypress/fixtures/images/large-size-image.jpg", {
+    getImageField().selectFile("cypress/fixtures/images/large-size-image.jpg", {
       force: true,
     });
-    formSubmitButton.click();
+    getFormSubmitButton().click();
 
     cy.getByData("input-error")
       .last()
@@ -124,3 +53,66 @@ describe("signup-page-form", () => {
 
   // it("Check if errors go away once the user has entered valid inputs", () => {})
 });
+
+
+// Helper functions
+const getNameField = () => cy.getByData("name-input");
+const getEmailField = () => cy.getByData("email-input");
+const getPasswordField = () => cy.getByData("password-input");
+const getConfirmPasswordField = () => cy.getByData("confirmPassword-input");
+const getDobField = () => cy.getByData("dob-input");
+const getTagNameField = () => cy.getByData("tagName-input");
+const getUserDescriptionField = () => cy.getByData("userDescription-input");
+const getTagSelectField = () => cy.getById("tagSelect-input");
+const getImageField = () => cy.getByData("image-input");
+const getFormSubmitButton = () => cy.getByData("signupSubmit-btn");
+
+function checkErrorMessages(
+  inputErrors: Cypress.Chainable<JQuery<HTMLElement>>,
+  expectedErrors: string[]
+) {
+  inputErrors.each(($el, index) => {
+    cy.wrap($el).invoke("text").should("eq", expectedErrors[index]);
+  });
+}
+
+function fillInvalidSignupInput() {
+  const futureDate = new Date();
+  futureDate.setFullYear(futureDate.getFullYear() + 1);
+
+  // Setting incorrect values to all inputs
+  getNameField().type("Mi");
+  getEmailField().type("jo.com@gmail");
+  getPasswordField().type("123123123123123123123");
+  getConfirmPasswordField().type("1234567");
+  getDobField().type(returnCypressDate(futureDate));
+  getTagNameField().type("mi");
+  getUserDescriptionField().type("Optional");
+  getTagSelectField().type("Tag 1{enter}");
+  getTagSelectField().type("Tag 2{enter}");
+  getImageField().selectFile("cypress/fixtures/images/bad-image.iso", {
+    force: true,
+  });
+
+  // These are the errors you can expect after filling the form with above values
+  const expectedFieldError = [
+    "String must contain at least 3 character(s)",
+    "Invalid email",
+    "String must contain at most 16 character(s)",
+    "String must contain at least 8 character(s)",
+    "Passwords do not match",
+    "Birth date must be in the past",
+    "String must contain at least 3 character(s)",
+    "You must select at least 3 tags.",
+    "Invalid file",
+    "Invalid image format",
+  ];
+  return expectedFieldError;
+}
+
+function returnCypressDate(date: Date) {
+  return `${date.getFullYear()}-${String(date.getMonth()).padStart(
+    2,
+    "0"
+  )}-${String(date.getMonth()).padStart(2, "0")}`;
+}
